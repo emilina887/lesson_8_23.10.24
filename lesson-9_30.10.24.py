75fb219ee5cfb69040a478ee4f472e7c7493d3e2
84764reui <emilinab9@gmail.com>
2024-10-30 17:28:50 +0200

    maip py ajuigwiuegiurw3642

--- lesson-9_30.10.24.py
+++ lesson-9_30.10.24.py
@@ -1,10 +1,31 @@
-size = 10
-choise_user = input("a, b, c... ")
-if choise_user == "a":
-    for i in range(size):
-        for j in range(size):
-            if i == 0 or i == size - 1 or j == 0 or j == size - 1 or i == j or j > i:
+# size = 10
+# choise_user = input("a, b, c... ")
+# if choise_user == "a":
+#     for i in range(size):
+#         for j in range(size):
+#             if i == 0 or i == size - 1 or j == 0 or j == size - 1 or i == j or j > i:
+#                 print("*", end="")
+#             else:
+#                 print(" ", end="")
+#         print()
+
+
+size = 4
+for k in range(size):
+    for i in range(9):
+        if i % 2 == 0:
+            for j in range(size):
                 print("*", end="")
-            else:
-                print(" ", end="")
-        print()
+        elif i % 2 == 1:
+            for j in range(size):
+                print("-", end="")
+    print()
+for k in range(size):
+    for i in range(9):
+        if i % 2 == 0:
+            for j in range(size):
+                print("-", end="")
+        elif i % 2 == 1:
+            for j in range(size):
+                print("*", end="")
+    print()
