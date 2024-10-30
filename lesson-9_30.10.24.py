59990c4958ee07d5f47033c9834814d695f25ae5
84764reui <emilinab9@gmail.com>
2024-10-30 18:02:54 +0200

    maip py ajuigwiuegiurw3642

--- lesson-9_30.10.24.py
+++ lesson-9_30.10.24.py
@@ -10,22 +10,100 @@
 #         print()
 
 
-size = 4
-for k in range(size):
-    for i in range(9):
-        if i % 2 == 0:
-            for j in range(size):
-                print("*", end="")
-        elif i % 2 == 1:
-            for j in range(size):
-                print("-", end="")
-    print()
-for k in range(size):
-    for i in range(9):
-        if i % 2 == 0:
-            for j in range(size):
-                print("-", end="")
-        elif i % 2 == 1:
-            for j in range(size):
-                print("*", end="")
-    print()
+# size = 4
+# for k in range(size):
+#     for i in range(9):
+#         if i % 2 == 0:
+#             for j in range(size):
+#                 print("*", end="")
+#         elif i % 2 == 1:
+#             for j in range(size):
+#                 print("-", end="")
+#     print()
+#     for i in range(9):
+#         if i % 2 == 0:
+#             for j in range(size):
+#                 print("-", end="")
+#         elif i % 2 == 1:
+#             for j in range(size):
+#                 print("*", end="")
+#     print()
+
+
+# size = 3
+# for k in range(size):
+#     for i in range(9):
+#         if i % 2 == 0:
+#             for j in range(size):
+#                 print("*", end="")
+#         elif i % 2 == 1:
+#             for j in range(size):
+#                 print("-", end="")
+#     print()
+
+
+# size = 4
+# for k in range(size):
+#     for i in range(size*2):
+#         if i % 2 == 0:
+#             for j in range(size):
+#                 print("*", end="")
+#         elif i % 2 == 1:
+#             for j in range(size):
+#                 print("-", end="")
+#     print()
+# for k in range(size):
+#     for i in range(size*2):
+#         if i % 2 == 0:
+#             for j in range(size):
+#                 print("-", end="")
+#         elif i % 2 == 1:
+#             for j in range(size):
+#                 print("*", end="")
+#     print()
+
+
+# from random import randint
+#
+# level = input('Level ')
+#
+# if level == "1":
+#     temp = 0
+#     for i in range(5):
+#         a = randint(1 , 10)
+#         b = randint(1 , 10)
+#         print(f"{a} {b}")
+#         res = int(input("Result "))
+#         if a * b == res:
+#             print('ok')
+#             temp += 1
+#         else:
+#             print('no')
+#     print(f"Grade {int(round(12 * temp / 5, 0))}")
+# elif level == "2":
+#     temp = 0
+#     for i in range(10):
+#         a = randint(1 , 10)
+#         b = randint(1 , 10)
+#         print(f"{a} {b}")
+#         res = int(input("Result "))
+#         if a * b == res:
+#             print('ok')
+#             temp += 1
+#         else:
+#             print('no')
+#     print(f"Grade {int(round(12 * temp / 10, 0))}")
+# elif level == "3":
+#     temp = 0
+#     for i in range(15):
+#         a = randint(1 , 10)
+#         b = randint(1 , 10)
+#         print(f"{a} {b}")
+#         res = int(input("Result "))
+#         if a * b == res:
+#             print('ok')
+#             temp += 1
+#         else:
+#             print('no')
+#     print(f"Grade {int(round(12 * temp / 15, 0))}")
+
