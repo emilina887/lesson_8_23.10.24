468188d46857d820fd854b184c9cf0cb6bf9b5bc
84764reui <emilinab9@gmail.com>
2024-11-06 18:55:36 +0200

    maip py ajuigwiuegiurw3642

---
+++ lesson-10_06.11.24.py
@@ -0,0 +1,57 @@
+# a = input("Enter: ")
+#
+# print(a[::-1])
+
+# my_str = "Hello World!"
+#
+# print(my_str.count(""))
+
+
+# str = input("Enter: ")
+# count_letters, count_number = 0, 0
+#
+# for i in range(0, len(str)):
+#     if str[i].isalpha():
+#         count_letters += 1
+#     if str[i].isnumeric():
+#         count_number += 1
+#
+# print(f"Letters {count_letters}, Number {count_number}")
+# print(len(str))
+
+
+# str = input("Enter: ")
+# count_letters, count_number = 0, 0
+#
+# for let in str:
+#     if let.isalpha():
+#         count_letters += 1
+#     if let.isnumeric():
+#         count_number += 1
+#
+# print(f"Letters {count_letters}, Number {count_number}")
+# print(len(str))
+
+
+# import time
+#
+# str = input("Enter: ")
+# count_letters, count_number = 0, 0
+# start = time.time()
+#
+# for let in str:
+#     if let.isalpha():
+#         count_letters += 1
+#     if let.isnumeric():
+#         count_number += 1
+#
+# print(f"Time {time.time() - start}")
+# print(f"Letters {count_letters}, Number {count_number}")
+# print(len(str))
+
+
+# str = input("Enter: ")
+#
+# print(str.count('із'))
+
+
